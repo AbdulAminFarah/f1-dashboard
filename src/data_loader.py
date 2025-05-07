24d34fb4dd4cc2976c8a33465a424b383308b655
AbdulAmin Farah <123128165+AbdulAminFarah@users.noreply.github.com>
2025-05-07 12:52:07 -0500

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,14 @@
+import fastf1
+import pandas as pd
+
+fastf1.Cache.enable_cache('data/fastf1_cache')
+
+def load_race_session(year, gp, session_type='R'):
+    session = fastf1.get_session(year, gp, session_type)
+    session.load()
+    return session
+
+def get_driver_laps(session, driver_code):
+    laps = session.laps.pick_driver(driver_code).pick_quicklaps()
+    return laps[['LapNumber', 'LapTime', 'Compound', 'PitOutTime', 'PitInTime']]
 
