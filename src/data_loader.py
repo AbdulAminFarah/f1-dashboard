a734f7413071429657ffbb49731d01aa643b0af2
AbdulAmin Farah <123128165+AbdulAminFarah@users.noreply.github.com>
2025-05-07 13:04:27 -0500

    Add get_stints ()

--- src/data_loader.py
+++ src/data_loader.py
@@ -12,3 +12,9 @@ def get_driver_laps(session, driver_code):
     laps = session.laps.pick_driver(driver_code).pick_quicklaps()
     return laps[['LapNumber', 'LapTime', 'Compound', 'PitOutTime', 'PitInTime']]
 
+#  Add get_stints()
+def get_stints(session, driver_code):
+    """Return tyre stint data for a driver."""
+    driver_laps = session.laps.pick_driver(driver_code)
+    stints = driver_laps.get_stint_info()
+    return stints[['Stint', 'Compound', 'StartLap', 'EndLap']]
